f477dfe3b40015d4eed91c9924385a616e99befb
MansurTiyes <tiyes06@mail.ru>
2023-01-08 19:32:43 +0000

    Added color mode, clear feature and changing of sizing corresponding to value on slider

--- script.js
+++ script.js
@@ -1,28 +1,66 @@
 const sizeSlider = document.querySelector('.sizeSlider');
 
+const colorMode = document.querySelector('.colorMode');
+colorMode.addEventListener('click', function(e){
+    hoverRemover();
+    colorMode.classList.add('hoverAnimation');
+    const colorPick = document.querySelector('.colorPick')
+    colorPick.addEventListener('input',function(e){
+        mainAction(colorPick.value);
+    })
+});
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click',function(e){
+    hoverRemover();
+    clear.classList.add('hoverAnimation');
+    const gridElement = document.querySelectorAll('.gridElement');
+    for (let i=0; i<gridElement.length; i++){
+        gridElement[i].setAttribute('style','background-color: white;');
+    }
+    mainAction('white');
+});
+
+/*const colorPick = document.querySelector('.colorPick')
+colorPick.addEventListener('input',function(e){
+    mainAction(colorPick.value);
+})*/
+
+/*const button = document.querySelectorAll('button');
+for (let i=0; i<button.length; i++){
+    button[i].addEventListener('click',function(e){
+        button[i].classList.add('hoverAnimation');
+    })
+}*/
+
 sizeSlider.addEventListener('input',function(e){
     gridRemover();
     gridMaker(sizeSlider.value);
+    sizeDisplayer(sizeSlider.value);
 });
 
-mainAction();
-
-function mainAction(){
+function mainAction(input){
     const gridElement = document.querySelectorAll(".gridElement");
     for(let i=0; i<gridElement.length; i++){
-        gridElement[i].addEventListener('click',function(e){
-            gridElement[i].classList.add('styleApply');
+        gridElement[i].addEventListener('mouseover',function(e){
+            gridElement[i].setAttribute('style',`background-color: ${input};`);
         })
     }
 }
 
 function gridMaker(input){
     const grid = document.querySelector('.grid');
-    for (let i =0; i<input; i++){
+    grid.setAttribute('style', `grid-template-columns: repeat(${input},1fr); grid-template-rows: repeat(${input},1fr);`);
+    for (let i =0; i<input**2; i++){
         const div = document.createElement('div');
         div.classList.add('gridElement');
         grid.appendChild(div);
     }
+}
+
+function sizeDisplayer(input){
+    const sizeValue = document.querySelector('.sizeValue');
+    sizeValue.textContent = input+"x"+input;
 }
 
 function gridRemover(){
@@ -32,3 +70,9 @@ function gridRemover(){
     }
 }
 
+function hoverRemover(){
+    const hoverAnimation = document.querySelectorAll('.hoverAnimation');
+    for(let i = 0; i<hoverAnimation.length; i++){
+        hoverAnimation[i].classList.remove('hoverAnimation');
+    }
+}
